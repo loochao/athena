c8f3f0463f6ed6438eeb953d43ee2380f8c13925
Channing <qianmao89@gmail.com>
2015-12-15 08:10:22 +0000

    integrate with mongoDB; add a small test;

--- node/nodeserver/routes/index.js
+++ node/nodeserver/routes/index.js
@@ -22,6 +22,50 @@ router.get('/export', function(req, res, next) {
   renderExport(req, res, next);
 });
 
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({}, {}, function(e, docs) {
+    res.render('userlist', {
+      "userlist" : docs
+    });
+  });
+});
+
+router.post('/adduser', function(req, res) {
+  // set internal DB variable
+  var db = req.db;
+
+  // get form values. These rely on the 'name' attribtues
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  console.log("userName + userEmail");
+
+  // set collection
+  var collection = db.get('usercollection');
+
+  // submit to the DB
+  collection.insert({
+    'username' : userName,
+    'email' : userEmail
+  }, function (err, doc) {
+    if (err) {
+      // if it failed, return error
+      console.log(err);
+      res.send("There was a problem adding the information to the database.");
+    } else {
+      // forward to success page
+      console.log("hello!!");
+      res.redirect("userlist");
+    }
+  });
+});
+
+router.get('/newuser', function(req, res) {
+  res.render('newuser', { title: 'Athena' });
+});
+
 function renderOverview(req, res, next) {
   res.render('overview', { title: 'Athena' });
 }
